bd4bf936bd785826b3dfe1cb5cfa976b68a791b2
Vivek <vivek1322420@gmail.com>
2023-03-15 22:32:05 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "7a27784dace660350d236dba8d567aae",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
