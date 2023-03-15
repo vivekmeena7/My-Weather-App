afa2cfd957d6cd03b5fd5037d0569d670f09e0b4
Vivek <vivek1322420@gmail.com>
2023-03-15 22:37:45 +0530

    Updates

---
+++ precache-manifest.0ae205f8fc951ef5223fde6d69451960.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5472e19392b85f60436a159cfb7e1761",
+    "url": "/My-Weather-App/index.html"
+  },
+  {
+    "revision": "5e8d39fbb207282c91d0",
+    "url": "/My-Weather-App/static/css/main.2815d4dc.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/My-Weather-App/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Weather-App/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8d39fbb207282c91d0",
+    "url": "/My-Weather-App/static/js/main.69bf95ad.chunk.js"
+  },
+  {
+    "revision": "fc585bb8d953475f5829",
+    "url": "/My-Weather-App/static/js/runtime-main.bd9a08f6.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/My-Weather-App/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/My-Weather-App/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/My-Weather-App/static/media/city.e1698332.jpg"
+  }
+]);
